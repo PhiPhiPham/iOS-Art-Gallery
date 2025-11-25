83805ff96773a567ca569aea149800be2d5b16cd
Phi Phi (Steven) Pham <112465192+PhiPhiPham@users.noreply.github.com>
2025-11-25 23:08:48 +0800

    Initial Commit

---
+++ iOS-Art-Gallery/iOS_Art_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Art_GalleryApp.swift
+//  iOS-Art-Gallery
+//
+//  Created by Phi Phi Pham on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Art_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
